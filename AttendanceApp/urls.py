e9aaea3dfdd45683a217979b24fd4d9216d26ddf
LiniEisha <linieesk@gmail.com>
2020-09-23 17:18:50 +0530

    IT17098960 is committing from IT17100908 user's branch.

--- AttendanceApp/urls.py
+++ AttendanceApp/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path, re_path, include
+from django.urls import path
+from .api import student_list, student_detail, subject_list, subject_detail, attendance_list, StudentAPIView, StudentDetails
 from django.conf.urls import url
-from . import views
+from .api import FileView
 
 urlpatterns = [
-    path('', views.first)
+    path('students/', student_list),
+    path('students/<str:pk>', student_detail),
+    path('subjects/', subject_list),
+    path('subjects/<str:pk>', subject_detail),
+    path('attendance/', attendance_list),
+    path('student/', StudentAPIView.as_view()),
+    # class based
+    path('student/', StudentAPIView.as_view()),
+    path('student/<str:pk>', StudentDetails.as_view()),
+    url(r'^upload/$', FileView.as_view(), name='file-upload')
 ]
--- AttendanceApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-def first(request):
-    return HttpResponse('<h1>Hello Attendance App</h1>')
